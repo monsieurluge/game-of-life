99095c2f5a6fcff83076100234d77e8361394712
Guillaume Aveline <guillaume@codr.fr>
2018-11-12 09:17:35 +0100

    project boilerplate

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "2g1c",
+  verbose: true,
+  roots: ["<rootDir>/src/", "<rootDir>/tests/"],
+  transform: {
+      "^.+\\.jsx?$": "babel-jest"
+  },
+}
---
+++ tests/index.test.js
@@ -0,0 +1,5 @@
+import {sum} from '../src'
+
+test('adds 1+1=2', () => {
+    expect(sum(1, 1)).toBe(2)
+})
