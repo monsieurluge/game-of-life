64e0a12f6c0e0859880cfeb1afb46d2387f3b93d
Guillaume Aveline <guillaume@codr.fr>
2018-11-16 10:21:35 +0100

    utilisation de describe pour séparer les testsuites des cells et organismes

--- tests/Organism.test.js
+++ tests/Organism.test.js
@@ -1,98 +1,100 @@
 import { Organism } from "../src/Organism";
 
-test('organism is defined', () => {
-    const organism = new Organism()
-    expect(organism).toBeDefined()
-})
+describe('we test organisms', () => {
+    test('organism is defined', () => {
+        const organism = new Organism()
+        expect(organism).toBeDefined()
+    })
 
-test('organism can spread', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dispatch: () => { timeCalled++ }        
-    }
+    test('organism can spread', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dispatch: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.spread()
-    expect(timeCalled).toBe(1)
-})
+        const organism = new Organism( mockedCell )
+        organism.spread()
+        expect(timeCalled).toBe(1)
+    })
 
-test('an living organism tries to eat spores', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        eatSpores: () => { timeCalled++ }        
-    }
+    test('an living organism tries to eat spores', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            eatSpores: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.live()
-    expect(timeCalled).toBe(1)
-})
+        const organism = new Organism( mockedCell )
+        organism.live()
+        expect(timeCalled).toBe(1)
+    })
 
-test('feed 3 spores => stay alive', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dying: () => { timeCalled++ }        
-    }
+    test('feed 3 spores => stay alive', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dying: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
+        const organism = new Organism( mockedCell )
 
-    organism.feed(3)
+        organism.feed(3)
 
-    expect(timeCalled).toBe(0)                
-})
+        expect(timeCalled).toBe(0)                
+    })
 
 
-test('feed 2 spores => die if first meal', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dying: () => { timeCalled++ }        
-    }
+    test('feed 2 spores => die if first meal', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dying: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.feed(2)
+        const organism = new Organism( mockedCell )
+        organism.feed(2)
 
-    expect(timeCalled).toBe(1)
-})
+        expect(timeCalled).toBe(1)
+    })
 
-test('feed 2 spores => do not die if not first meal', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dying: () => { timeCalled++ }        
-    }
+    test('feed 2 spores => do not die if not first meal', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dying: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.feed(3)
-    organism.feed(2)
+        const organism = new Organism( mockedCell )
+        organism.feed(3)
+        organism.feed(2)
 
-    expect(timeCalled).toBe(0)
-})
+        expect(timeCalled).toBe(0)
+    })
 
-test('feed 1 spore => die', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dying: () => { timeCalled++ }        
-    }
+    test('feed 1 spore => die', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dying: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.feed(1)
+        const organism = new Organism( mockedCell )
+        organism.feed(1)
 
-    expect(timeCalled).toBe(1)
-})
+        expect(timeCalled).toBe(1)
+    })
 
-test('feed 4 spore => die', () => {
-    let timeCalled = 0;
-    
-    const mockedCell = {
-        dying: () => { timeCalled++ }        
-    }
+    test('feed 4 spore => die', () => {
+        let timeCalled = 0;
+        
+        const mockedCell = {
+            dying: () => { timeCalled++ }        
+        }
 
-    const organism = new Organism( mockedCell )
-    organism.feed(4)
+        const organism = new Organism( mockedCell )
+        organism.feed(4)
 
-    expect(timeCalled).toBe(1)
+        expect(timeCalled).toBe(1)
+    })
 })
